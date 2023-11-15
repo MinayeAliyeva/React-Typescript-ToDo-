79244cad136414c16d50c73f67e20e2e64870888
MinayeAliyeva <7s346pu@code.edu.az>
2023-11-15 22:11:45 +0400

    deleted all

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,58 +1,12 @@
-import { ChangeEvent, FC } from "react";
+
 import "./App.css";
-import { useState } from "react";
-import { todoType } from "./appTypes";
-import TodoItem from "./TodoItem";
-import { uid } from "uid";
 
-const App: FC = () => {
-  //taskin tipi string
-  const [task, setTask] = useState<string>("");
-  const [workDay, setWorkDay] = useState<number>(0);
-  const [toDos, setToDos] = useState<todoType[]>([]);
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    console.log(typeof event.target.value);
 
-    if (event.target.name === "task") {
-      setTask(event.target.value);
-    } else {
-      setWorkDay(Number(event.target.value));
-    }
-  };
-  const addNewTask = (): void => {
-    const newTask = {
-      taskName: task,
-      workDay,
-    };
-    setToDos([...toDos, newTask]);
-    setTask("");
-    setWorkDay(0);
-  };
-  const deleteTask = (name: string): void => {
-    setToDos(toDos.filter((task) => task.taskName !== name));
-  };
+const App = () => {
+
   return (
     <div className="App">
-      <div>
-        <input
-          name="task"
-          value={task}
-          onChange={handleChange}
-          type="text"
-          placeholder="Taskinizi giriniz..."
-        />
-        <input
-          name="workDay"
-          value={workDay}
-          onChange={handleChange}
-          type="number"
-          placeholder="Kac gunde tamamlamalisiniz..."
-        />
-        <button onClick={addNewTask}>Yeni Task Ekle</button>
-      </div>
-      {toDos.map((task: todoType) => {
-        return <TodoItem deleteTask={deleteTask} key={uid()} task={task} />;
-      })}
+ 
     </div>
   );
 };
--- my-app/src/appTypes.tsx
+++
@@ -1,4 +0,0 @@
-export type todoType = {
-    taskName: string;
-    workDay: number;
-  };
